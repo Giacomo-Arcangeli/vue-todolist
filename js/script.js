a84eb11d3b3897719e242ec034bd5161deabdddc
Giacomo-Arcangeli <giacomoarcangeli1997@gmail.com>
2023-06-20 16:19:05 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Estrapolo il metodo createApp
+const { createApp } = Vue;
+
+// Inizializzo l'app Vue
+const app = createApp({
+    data() {
+        return {
+            
+        }
+    }
+});
+
+// La monto nell'elemento HTML "root"
+app.mount('#root');
